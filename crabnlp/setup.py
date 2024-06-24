0b56cc0eda7c9ea0e2d7bd4c3c3cc6f3cc477f1b
Irina Lebedeva <nixe.irsen@gmail.com>
2024-06-24 18:36:23 +0300

    README, setup

--- crabnlp/setup.py
+++ crabnlp/setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
-  
+
 setup(
     name='crabnlp',
     version='1.0.0',
     description='NLP routines',
     packages=['crabnlp'],
     install_requires=[
-        'openai'
+        'openai',
+        'pytube',
+        'transformers',
+        'PyICU',
+        'pycld2',
+        'polyglot',
+        'tiktoken',
+        'python-json-logger',
+        'iso-639',
+        'faster_whisper==0.5.0',
+        'pyannote.audio==2.1.1'
     ],
 )
